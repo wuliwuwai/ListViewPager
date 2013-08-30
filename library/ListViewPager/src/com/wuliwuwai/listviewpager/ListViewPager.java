8d754e3b3a60035cc6762050f4b11076cc451c94
wuliwuwai <315331371@qq.com>
2013-08-31 01:35:40 +0800

    加载控件由移除改成隐藏

--- library/ListViewPager/src/com/wuliwuwai/listviewpager/ListViewPager.java
+++ library/ListViewPager/src/com/wuliwuwai/listviewpager/ListViewPager.java
@@ -3,19 +3,36 @@ package com.wuliwuwai.listviewpager;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import android.content.Context;
-import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.AbsListView;
 import android.widget.AbsListView.OnScrollListener;
-import android.widget.LinearLayout;
 import android.widget.ListView;
 
 public class ListViewPager {
+    public interface PagerService {
+        /**
+         * 
+         * @param page
+         *            页码，通常情况下page++
+         * @param finished
+         *            当回调接口执行完成后，调用finished里的回调借口onFinished
+         */
+        public void getNext(int page, OnServiceFinished finished);
+
+    }
+
+    public interface OnServiceFinished {
+        /**
+         * 客户端获得数据后必须调用的方法
+         */
+        public void onFinished();
+    }
 
     /**
      * 加载分页
      */
-    private LinearLayout     loadingLayout;
+    private View             loadingView;
     /**
      * 当前页
      */
@@ -24,10 +41,7 @@ public class ListViewPager {
      * 有更多
      */
     private Boolean          hasMore         = true;
-    /**
-     * 当前现实最后一个item
-     */
-    private Integer          lastItem        = 0;
+
     /**
      * 滑动事件
      */
@@ -40,93 +54,30 @@ public class ListViewPager {
     /**
      * 加载的ListView控件
      */
-    private ListView         listView;
-
-    private PagerService     pagerService;
-
-    public interface PagerService {
-        /**
-         * 
-         * @param page
-         *            页码，通常情况下page++
-         * @param finished
-         *            当回调接口执行完成后，调用finished里的回调借口onFinished
-         */
-        public void getNext(int page, OnServiceFinished finished);
-    }
-
-    public interface OnServiceFinished {
-        /**
-         * service执行完成之后需要调用
-         */
-        public void onFinished();
-    }
-
-    public void removeFootView() {
-        this.listView.removeFooterView(loadingLayout);
-    }
-
-    public void reset() {
-        // removeFootView();
-        this.page = 1;
-        this.lastItem = 0;
-        this.hasMore = true;
-        keepOnAppending.set(true);
-    }
-
-    public void requestData() {
-
-    }
-
+    private ListView         mListView;
     /**
-     * pull to refresh
-     * 
-     * @param context
-     * @param mListView
-     * @param isPullToRefresh
-     * @param service
+     * 回调接口
      */
-    public ListViewPager(Context context, ListView mListView,
-            boolean isPullToRefresh, final PagerService service) {
-        super();
-        if (isPullToRefresh) {
-            reset();
-            this.listView = mListView;
-            loadingLayout = (LinearLayout) LayoutInflater.from(context)
-                    .inflate(R.layout.view_loading_layout, null);
+    private PagerService     pagerService;
+    /**
+     * 上下文对象
+     */
+    private Context          mContext;
 
-            this.listView.addHeaderView(loadingLayout);
-            onScrollListener = new OnScrollListener() {
-                @Override
-                public void onScrollStateChanged(AbsListView view,
-                        int scrollState) {
-                }
-
-                @Override
-                public void onScroll(AbsListView view, int firstVisibleItem,
-                        int visibleItemCount, int totalItemCount) {
-
-                    if (hasMore && keepOnAppending.get()) {
-
-                        // lastItem = firstVisibleItem + visibleItemCount - 1;
-                        if (firstVisibleItem == 0) {
-                            getPullData(service);
-                        }
-                    }
-                }
-            };
-            this.listView.setOnScrollListener(onScrollListener);
-        }
+    public ListViewPager(Context context, ListView listView,
+            PagerService pagerService) {
+        this(context, listView);
+        this.setPagerService(pagerService);
     }
 
-    public ListViewPager(Context context, ListView mListView,
-            final PagerService service) {
-        reset();
-
-        this.listView = mListView;
-        loadingLayout = (LinearLayout) LayoutInflater.from(context).inflate(
+    public ListViewPager(Context context, ListView listView) {
+        super();
+        reSet();
+        this.mContext = context;
+        this.mListView = listView;
+        loadingView = LayoutInflater.from(this.mContext).inflate(
                 R.layout.view_loading_layout, null);
-        this.listView.addFooterView(loadingLayout);
+        this.mListView.addFooterView(loadingView);
         onScrollListener = new OnScrollListener() {
             @Override
             public void onScrollStateChanged(AbsListView view, int scrollState) {
@@ -137,58 +88,71 @@ public class ListViewPager {
                     int visibleItemCount, int totalItemCount) {
 
                 if (hasMore && keepOnAppending.get()) {
-                    lastItem = firstVisibleItem + visibleItemCount - 1;
                     if (firstVisibleItem + visibleItemCount == totalItemCount) {
-                        getData(service);
+                        if (pagerService != null) {
+                            final int pCount = mListView.getCount();
+                            keepOnAppending.set(false);
+                            pagerService.getNext(page++,
+                                    new OnServiceFinished() {
+                                        @Override
+                                        public void onFinished() {
+                                            // 获取下一页数据后，数据的个数没有变化表示没有心的数据，隐藏加载控件
+                                            if (mListView.getCount() == pCount) {
+                                                hasMore = false;
+                                                // listView.removeFooterView(loadingView);
+                                                loadingView
+                                                        .setVisibility(View.GONE);
+                                            }
+                                            keepOnAppending.set(true);
+                                        }
+                                    });
+                        }
                     }
                 }
             }
         };
-        this.listView.setOnScrollListener(onScrollListener);
-
+        this.mListView.setOnScrollListener(onScrollListener);
     }
 
-    private void getPullData(PagerService service) {
-        final int pCount = listView.getCount();
-        keepOnAppending.set(false);
-        service.getNext(page++, new OnServiceFinished() {
-            @Override
-            public void onFinished() {
-
-                Log.i("ListViewPager", pCount + "   " + listView.getCount()
-                        + " footViewCount:" + listView.getFooterViewsCount()
-                        + " childCount:" + listView.getCount());
-                if (listView.getCount() == pCount) {
-                    hasMore = false;
-
-                    listView.removeHeaderView(loadingLayout);
-
-                }
-                keepOnAppending.set(true);
-            }
-        });
+    /**
+     * 移除底部控件
+     */
+    public void removeFootView() {
+        this.mListView.removeFooterView(loadingView);
     }
 
-    private void getData(PagerService service) {
-        final int pCount = listView.getCount();
-        keepOnAppending.set(false);
-        service.getNext(page++, new OnServiceFinished() {
-            @Override
-            public void onFinished() {
+    /**
+     * 调用这个方法可以完成刷新，客户端代码需要同时清空掉ListView适配器中的数据
+     * 
+     * 
+     */
+    public void reSet() {
+        if (loadingView != null) {
+            this.loadingView.setVisibility(View.VISIBLE);
+        }
+        this.page = 1;
+        this.hasMore = true;
+        keepOnAppending.set(true);
+    }
 
-                Log.i("ListViewPager", pCount + "   " + listView.getCount()
-                        + " footViewCount:" + listView.getFooterViewsCount()
-                        + " childCount:" + listView.getCount());
-                if (listView.getCount() == pCount) {
-                    hasMore = false;
-                    // if (listView.getFooterViewsCount() > 0)
-                    // listView.getChildCount();
-                    // loadingLayout.setVisibility(View.GONE);
-                    listView.removeFooterView(loadingLayout);
-                }
-                keepOnAppending.set(true);
-            }
-        });
+    public Integer getPage() {
+        return page;
+    }
+
+    public Boolean getHasMore() {
+        return hasMore;
+    }
+
+    public View getLoadingView() {
+        return loadingView;
+    }
+
+    public void setLoadingView(View loadingView) {
+        this.loadingView = loadingView;
+    }
+
+    public void setPagerService(PagerService pagerService) {
+        this.pagerService = pagerService;
     }
 
 }
